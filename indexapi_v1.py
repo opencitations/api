61906f0f372280bcaf83b224ff3f8943ef00407c
ivan.heibi <ivanhb.ita@gmail.com>
2023-09-11 18:05:26 +0200

    testing ...

--- indexapi_v1.py
+++ indexapi_v1.py
@@ -177,7 +177,7 @@ def citations_info(res, *args):
 
     all_entities = {"br/061302130471"}
     # ["id", "author", "year", "pub_date", "title", "source_title", "volume", "issue", "page", "source_id"]
-    #r = __ocmeta_parser(list(all_entities),"omid")
+    r = __ocmeta_parser(list(all_entities),"omid")
 
     # process and elaborate additional fields
     #creation = entities_data["citing"][1]
