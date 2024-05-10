5b2e097e67f7b2ede2328fc20e519ddfa2095090
ivanhb <utente@ZA-FICLIT-D070A.local>
2024-05-10 12:32:38 +0200

    test journal citation count

--- indexapi_v2.py
+++ indexapi_v2.py
@@ -45,9 +45,11 @@ def __get_omid_of(s, multi = False):
     sparql_endpoint = "http://127.0.0.1:3003/blazegraph/sparql"
 
     # SPARQL query
+    is_journal = False
     br_pre_l = ["doi","issn","isbn","pmid","pmcid","url","wikidata","wikipedia","jid","arxiv"]
     for br_pre in br_pre_l:
         if s.startswith(br_pre+":"):
+            is_journal = br_pre in ["issn"]
             s = s.replace(br_pre+":","")
             break
 
@@ -58,6 +60,26 @@ def __get_omid_of(s, multi = False):
             ?identifier literal:hasLiteralValue '"""+s+"""'.
             ?br datacite:hasIdentifier ?identifier;}
     """
+
+    # in case is a journal the SAPRQL query retrieves all associated BRs
+    if is_journal:
+        sparql_query = """
+            PREFIX datacite: <http://purl.org/spar/datacite/>
+            PREFIX literal: <http://www.essepuntato.it/2010/06/literalreification/>
+            PREFIX ns1: <http://purl.org/vocab/frbr/core#>
+            PREFIX fabio: <http://purl.org/spar/fabio/>
+            SELECT ?item {
+            	?identifier literal:hasLiteralValue '"""+s+"""' .
+            	?venue datacite:hasIdentifier ?identifier .
+              	# all BRs of a Journal
+              	{?item ns1:partOf ?venue .}
+              	UNION { ?item ns1:partOf/ns1:partOf ?venue . }
+              	UNION { ?item ns1:partOf/ns1:partOf/ns1:partOf ?venue . }
+              	UNION { ?item ns1:partOf/ns1:partOf/ns1:partOf/ns1:partOf ?venue .}
+              	?item a fabio:JournalArticle .
+            }
+        """
+
     headers={"Accept": "application/sparql-results+json", "Content-Type": "application/sparql-query"}
     data = {"query": sparql_query}
     omid_l = []
@@ -147,7 +169,6 @@ def citations_info(res, *args):
 
     # process and elaborate additional fields
     #creation = entities_data["citing"][1]
-    index_cits = set()
     if len(res) > 1:
         for idx, row in enumerate(res[1:]):
 
@@ -188,29 +209,22 @@ def citations_info(res, *args):
             #     else:
             #         pre_source = "poci => "
 
-            # create all possible pairs of any-id in the citation
-            cit_pairs = [(a, b) for a in citing_id.split(" ") for b in cited_id.split(" ")]
-
-            if not(any(c in index_cits for c in cit_pairs)):
-                row.extend([
-                    # oci value
-                    oci_val,
-                    # citing
-                    citing_id,
-                    # cited
-                    cited_id,
-                    # creation = citing[pub_date]
-                    citing_pubdate,
-                    # timespan = citing[pub_date] - cited[pub_date]
-                    duration,
-                    # journal_sc = compare citing[source_id] and cited[source_id]
-                    journal_sc,
-                    # author_sc = compare citing[source_id] and cited[source_id]
-                    author_sc
-                ])
-
-            # update the index of all citations
-            index_cits.update( cit_pairs )
+            row.extend([
+                # oci value
+                oci_val,
+                # citing
+                citing_id,
+                # cited
+                cited_id,
+                # creation = citing[pub_date]
+                citing_pubdate,
+                # timespan = citing[pub_date] - cited[pub_date]
+                duration,
+                # journal_sc = compare citing[source_id] and cited[source_id]
+                journal_sc,
+                # author_sc = compare citing[source_id] and cited[source_id]
+                author_sc
+            ])
 
 
     return res, True
